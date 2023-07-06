b2d7ee4c6cb1e7ba30e66c9f0fdc28078d55c67a
Emmanuel Herrera <emherrer1@gmail.com>
2023-07-06 16:03:25 -0400

    add url endpoint for yearly and all data

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
 import pandas as pd
 
+
 app = Flask(__name__)
 
+stations = pd.read_csv("data/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
 
-data = pd.read_csv("data/stations.txt", skiprows=17)
-data = data[["STAID", "STANAME                                 "]]
+# Home
 
 
 @app.route("/")
 def home():
-    return render_template("home.html", data=data.to_html())
+    return render_template("home.html", data=stations.to_html())
+
+# One station for one date
+
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
@@ -20,6 +25,29 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+# One station for all dates
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filepath = "data//TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+# One station for one year
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def one_year(station, year):
+    filepath = "data//TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filepath, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(
+        str(year))].to_dict(orient="records")
+    return result
+
 
 if __name__ == "__main__":
     app.run(debug=True)
